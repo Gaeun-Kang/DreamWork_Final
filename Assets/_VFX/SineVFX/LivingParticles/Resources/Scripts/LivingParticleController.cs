e9d3e28a67f0dab8107dd013ef8f1cad3bd4810c
Gaeun-Kang <1805gaeun@gmail.com>
2026-05-22 20:34:56 +0900

    cool Visual Effect Upload

---
+++ Assets/_VFX/SineVFX/LivingParticles/Resources/Scripts/LivingParticleController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystemRenderer))]
+public class LivingParticleController : MonoBehaviour
+{
+    public Transform affector;
+
+    private ParticleSystemRenderer psr;
+    private MaterialPropertyBlock propertyBlock;
+
+    private static readonly int AffectorID = Shader.PropertyToID("_Affector");
+
+    void Awake()
+    {
+        psr = GetComponent<ParticleSystemRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    void LateUpdate()
+    {
+        if (affector == null || psr == null) return;
+
+        psr.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetVector(AffectorID, new Vector4(
+            affector.position.x,
+            affector.position.y,
+            affector.position.z,
+            0f
+        ));
+        psr.SetPropertyBlock(propertyBlock);
+    }
+}
