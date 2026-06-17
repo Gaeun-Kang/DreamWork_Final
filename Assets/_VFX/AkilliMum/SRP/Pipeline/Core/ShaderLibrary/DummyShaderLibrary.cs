8efd8a7586084c9f25a7a65bcc6bfd3ac024a531
Gaeun-Kang <1805gaeun@gmail.com>
2026-06-18 08:07:32 +0900

    Clean UP

--- Assets/_VFX/AkilliMum/SRP/Pipeline/Core/ShaderLibrary/DummyShaderLibrary.cs
+++
@@ -1,7 +0,0 @@
-// This file is only to force Unity to load the ShaderLibrary's hlsl files in visual studio project via asmdef file, so they can be browse.
-namespace AkilliMum
-{
-    class DummyShaderLibrary
-    {
-    }
-}
--- Assets/_VFX/AkilliMum/SRP/Pipeline/Shaders/Shaders.cs
+++
@@ -1,8 +0,0 @@
-// UniversalRP Shaders folder need an Assembly Definition files to create a .cs project
-// This is a dummy shader file so the Shaders assembly is not empty.
-namespace AkilliMum
-{
-    class ShadersDummy
-    {
-    }
-}
